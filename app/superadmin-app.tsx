6cad4108ed8fa8db7d936e06821150978ff0b548
alex osorio <ale.osoriov@duocuc.cl>
2026-07-17 20:16:17 -0400

    refactor: retirar consolas antiguas dependientes del vault

--- app/superadmin-app.tsx
+++ app/superadmin-app.tsx
@@ -1,15 +1,7 @@
 "use client";
 
 import GestionVerdeApp from "./gestionverde-app";
-import SuperadminConsole from "./superadmin-console";
-import SuperadminRouteManager from "./superadmin-route-manager";
 
 export default function SuperadminApp() {
-  return (
-    <>
-      <SuperadminConsole />
-      <SuperadminRouteManager />
-      <GestionVerdeApp role="superadmin" />
-    </>
-  );
+  return <GestionVerdeApp role="superadmin" />;
 }
